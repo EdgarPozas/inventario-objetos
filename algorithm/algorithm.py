99fdb6df94f4d03282811112c6f38ecb59899df1
Edgar Pozas <edgar730a@gmail.com>
2021-04-15 11:44:31 -0600

    not ideal

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -1,58 +1,51 @@
 import random
 
-def listSub_(target,dataSet,path,min_):
+ends={}
+
+def listSub(originalTarget,target,dataSet,path):
     if len(dataSet)==0:
-        return path,min_
-    
-    for x in dataSet:
-        t=target-x
-        print(target,"-",x,"=",t)
-        af=list(filter(lambda it:it<=t,dataSet))
-        print(t,af,path+[x],min_)
-        # p,m=listSub_(t,af,path+[x],min_)
-        # print(p,m)
+        # print("->",path,originalTarget-sum(path))
+        k=abs(originalTarget-sum(path))
+        if not k in ends.keys():
+            ends[k]=[]
+        ls=list(path)
+        ls.sort()
+        if not ls in ends[k]:
+            ends[k].append(ls)
+        return
 
-
-def listSub(target,dataSet):
-    return listSub_(target,dataSet,[],0)
-    
-        # return path,min
-
-    # minRest=-1
-    # for i in range(0,len(dataSetFiltered)):
-    #     pivot=dataSetFiltered[i]
-    #     tmpList=[pivot]
-    #     for e in range(i+1,len(dataSetFiltered)):
-    #         tmpPivot=dataSetFiltered[e]
-    #         summatory=sum(tmpList)+tmpPivot
-    #         rest=target-summatory
-    #         print(tmpList,tmpPivot,"->",summatory,rest)
-    #         # if summatory>target:
-    #         #     break
-    #         if summatory<target:
-    #             tmpList.append(tmpPivot)
-    #         #     # if rest >0:
-    #         #     #     if minRest==-1:
-    #         #     #         minRest=rest
-    #         #     #     else:
-    #         #     #         if rest < minRest:
-    #         #     #             minRest=rest
-    #         # elif summatory==target:
-    #         #     tmpList.append(tmpPivot)
-    #         #     print("->",tmpList,"=",target)
-    #         #     lists.append(list(tmpList))
-    #         #     tmpList.pop()
-
-    return lists
+    for i in range(0,len(dataSet)):
+        value=dataSet[i]
+        arr=dataSet[0:i]+dataSet[i+1:]
+        t=target-value
+        arrF=list(filter(lambda it:it<=t,arr))
+        path=path+[value]
+        # print(target,"-",value,"=",t,"\t",arr,"<="+str(t),arrF,path)
+        
+        listSub(originalTarget,t,arrF,path)
+        p=path.pop()
+        # print("pop",p)
+        # print(path)
 
 if __name__ == "__main__":
-    size=100000
+    size=10
     limitMin=0
     limitMax=40
 
     target=153
-    # dataSet=[random.randint(limitMin,limitMax) for x in range(0,size)]
+    dataSet=[random.randint(limitMin,limitMax) for x in range(0,size)]
     # dataSet=[36, 21, 8, 12, 33]
-    dataSet=[70,100,20,50,120,80]
+    # dataSet=[70,100,20,70,50,120,80,160]
     # print(dataSet)
-    print(listSub(target,dataSet))
+    listSub(target,target,dataSet,[])
+    print(ends)
+    # min_=-1
+    # for x in ends:
+    #     if min_==-1:
+    #         min_=x["rest"]
+    #     else:
+    #         if x["rest"]<min_:
+    #             min_=x["rest"]
+        
+    # subs=list(filter(lambda x: x["rest"]==min_,ends))
+    # print(subs)
